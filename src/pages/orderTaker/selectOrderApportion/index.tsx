0a304b21348ce23584e4a1d7040cfcabf8c08971
buweiling <bwlloveistrue@163.com>
2020-04-21 22:58:06 +0800

    commit

---
+++ src/pages/orderTaker/selectOrderApportion/index.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { connect } from 'dva';
+
+@connect(({ loading }: { loading: { effects: { [key: string]: boolean } } }) => ({
+  submitting: loading.effects['formAndadvancedForm/submitAdvancedForm'],
+}))
+class SelectOrderApportion extends Component {
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <PageHeaderWrapper content="高级表单常见于一次性输入和提交大批量数据的场景。"></PageHeaderWrapper>
+    );
+  }
+}
+
+export default SelectOrderApportion;
